c80651b048fee431a345247052255545cf1e9fd6
rdottle <rachaelcdottle@gmail.com>
2019-03-23 17:30:55 -0400

    proj setup

---
+++ src/app.js
@@ -0,0 +1,3 @@
+import './style/app.css';
+
+console.log('helo');
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const pug = {
+  test: /\.pug$/,
+  use: ['html-loader?attrs=false', 'pug-html-loader']
+};
+
+const css = {
+  test: /\.css$/,
+    use: [
+      'style-loader',
+      'css-loader'
+    ]
+};
+
+const config = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [pug, css]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.pug',
+      inject: false
+    })
+ ],
+ devServer: {
+    contentBase: './dist'
+},
+};
+module.exports = config;
